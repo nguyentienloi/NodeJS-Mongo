e65158669953145ebf8625187680462398d656e3
nguyentienloi <nguyentienloi>
2019-11-20 08:37:15 +0700

    create project nodejs vs mongodb

---
+++ index.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dboper = require('./operations');
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'conFusion';
+
+MongoClient.connect(url, (err, client) => {
+
+    assert.equal(err, null);
+
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbname);
+    dboper.insertDocument(db,  { name: "Vadonut", description: "Test"}, 'dishes', (result) => {
+        console.log('Insert Document:\n', result.ops);
+
+        dboper.findDocuments(db, 'dishes', (docs) => {
+            console.log('Found Documents:\n', docs);
+
+            dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Update test'}, 'dishes', (result) => {
+                console.log('Update Document:\n', result.result);
+
+                dboper.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found Documents:\n', docs);
+
+                    db.dropCollection('dishes', (result) => {
+                        console.log('Dropped Collection:', result);
+
+                        client.close();
+                    });
+                });
+            });
+        });
+    });
+});
